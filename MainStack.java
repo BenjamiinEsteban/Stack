153727adb9ca7cf2797df72ce6da23c3455409eb
BenjamiinEsteban <benjaaa15@gmail.com>
2016-05-04 12:51:01 -0300

    Added files via upload

---
+++ MainStack.java
@@ -0,0 +1,14 @@
+package ejerciciospila;
+
+
+public class EjerciciosPila {
+
+    public static void main(String[] args) {
+        Respuestas jojo=new Respuestas();
+        jojo.e_1();
+        jojo.e_2();
+        jojo.e_3();
+        jojo.e_4();
+    }
+    
+}
